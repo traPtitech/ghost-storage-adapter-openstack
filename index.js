a025ab855b486e76c70c8b9ffeacbbe170f6c195
sapphi-red <green@sapphi.red>
2021-02-25 23:40:10 +0900

    Fix: Input buffer contains unsupported image format

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ class OpenstackAdapter extends BaseAdapter {
 
     this.containerName = config.container
     this.serverUrl = config.serverUrl
+
     this.client = pkgcloud.storage.createClient({
       provider: 'openstack',
       username: config.username,
@@ -23,6 +24,11 @@ class OpenstackAdapter extends BaseAdapter {
       tenantId: config.tenantId,
       container: config.container
     })
+    // https://github.com/lovell/sharp/issues/930
+    this.client.before.push(function (req) {
+      req.encoding = null
+    })
+
     this.cache = new Cache(config.cacheFolder, this.client, this.containerName)
   }
 
